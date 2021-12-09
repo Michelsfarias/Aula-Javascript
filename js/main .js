630b7ba090d659652923cf22f37854dc407d95ab
Michel <michelsfarias90@gmail.com>
2021-12-09 11:39:10 -0300

    alterações

--- js/main .js
+++ js/main .js
@@ -1,1 +1,29 @@
-alert("meu primeiro js");
+
+
+
+
+
+//var frutas = [{nome: "pera", cor:"Verde"}, {nome: "Banana", cor:"Amarela"}];
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+
+//var lista = ["maça", "pera", "uva"];
+//lista.push("Laranja");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" / "));
+//console.log(lista.reverse());
+//console.log(lista.length);
+//alert(lista[1])
+
+//var nome = "Michel SFarias";
+//var n1 = 5;
+//var n2 = 4;
+//var frase = "Brasil é o melhor time do mundo!";
+//alert ("Bem vindo" + nome + "Você tem" + idade + "anos");
+//alert(idade + idade2);
+//console.log (nome);
+//console.log(n1 * n2);
+//console.log (frase.replace("Brasil" , "França"))
